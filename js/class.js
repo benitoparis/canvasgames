b6a754eb5652277ff6a65260614aabbb4849439d
benitoparis <34385338+benitoparis@users.noreply.github.com>
2019-06-05 23:24:08 +0200

    MAJ
    MAJ

---
+++ js/class.js
@@ -0,0 +1,3 @@
+
+
+console.log('c\'est moi class');
--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
-console.log("hello");
-
 // Déclaration des variables
 const stage = document.getElementById("stage");
-stage.width = window.innerWidth;
-stage.height = window.innerHeight;
+/*stage.width = window.innerWidth;
+stage.height = window.innerHeight;*/
+stage.width = 640;
+stage.height= 360;
 let innerWidth = window.innerWidth;
 let innerHeight = window.innerHeight;
-let fps = 60;
+let fps = 10;
 const charImg = new Image();
 const tileFloorHouseImg = new Image();
 const tileChairHouseImg = new Image();
+const backgroundImg = new Image();
 
 
 
-// classe d héro
+// classe du héro
  class Hero {
 
  	// Constructeur de la classe du héro
@@ -26,6 +27,10 @@ const tileChairHouseImg = new Image();
  		this.height = h;
  		this.speedX = speedX;
  		this.speedY = speedY;
+ 		this.faceX = 70;
+ 		this.faceY = 207;
+		this.centerX = ((this.x + this.width) - (this.width / 2));
+ 		this.centerY = ((this.y + this.height) - (this.height / 2));
  		// this.update = this.update.bind(this);
  
 
@@ -35,7 +40,10 @@ const tileChairHouseImg = new Image();
 	// Méthode pour afficher le sprite du héro
 	drawHero() {
 		console.log('ou la', this.x);
-		ctx.drawImage(charImg, 70 , 207 , 74 , 95, this.x, this.y, this.width , this.height);
+		ctx.drawImage(charImg, this.faceX , this.faceY , 74 , 95, this.x, this.y, this.width , this.height);
+
+		console.log('this.centerX', this.centerX);
+
 
 	}
 
@@ -48,35 +56,107 @@ const tileChairHouseImg = new Image();
 		console.log('la', this.x);
 
 		switch (event.key) {
-			case "6":
+			case "6": // droite
 				console.log('ici', this.x);
 				console.log('A droite');
-				this.x = (this.x + this.speedX);
+				
 				console.log('this.x', this.x);
+				
+				if(this.faceY === 310) {
+					this.speedX = 20;
+					this.speedY = 20;
+				}
+				this.x = (this.x + this.speedX);
+
+				this.faceX = 8;
+				this.faceY = 120;
+
 				break;
 
 			case "4":
 				console.log('A gauche');
+				
+
+				if(this.faceY === 120) {
+					this.speedX = 20;
+					this.speedY = 20;
+				}
 				this.x = this.x - this.speedX;
+				this.faceX = 8;
+				this.faceY = 310;
 				break;
 			
 			case "8":
 				console.log('En haut');
+				
+
+				if(this.faceY === 210) {
+					this.speedX = 20;
+					this.speedY = 20;
+				}
 				this.y = this.y - this.speedY;
+				this.faceX = 8;
+				this.faceY = 22;
 				break;
 
 			case "2":
 				console.log('En bas');
+				
+
+				if(this.faceY === 22) {
+					this.speedX = 20;
+					this.speedY = 20;
+				}
 				this.y = this.y + this.speedY;
+				this.faceX = 8;
+				this.faceY = 210;
 				break;
 
 			default:
 				break;
 		}
+
+		// On recalcle le centre du héro
+		this.centerX = ((this.x + this.width) - (this.width / 2));
+ 		this.centerY = ((this.y + this.height) - (this.height / 2));
 	}
 	
 
  }
+
+
+// classe du héro
+class Obstacles {
+
+ 	// Constructeur de la classe des Obstacles
+ 	constructor(x, y, w, h) {
+ 		this.x = x;
+ 		this.y = y;
+ 		this.width = w;
+ 		this.height = h;
+  	}
+
+}
+
+
+const obstacle =  new Obstacles(412, 65, 66, 55);
+
+// Méthode pour vérifier la collision entre un élément a et un objet b
+const checkCollision = (a, b) => {
+
+	console.log("test", b.centerX);
+	console.log('obstacle.x', a.x);
+
+	if((a.x < b.centerX) && (b.centerX < (a.x + a.width))
+	 && (a.y < b.centerY)
+	 && (b.centerY < (a.y + a.height))) {
+		 return true;
+	} else {
+		return false;
+	}
+}
+
+
 
 const hero = new Hero (
 		100,
@@ -97,36 +177,55 @@ const ctx = stage.getContext("2d");
 // Méthode pour charger l'image des sprites d héros
 const loadImage = () => {
 	
-	      charImg.src = '../img/spritesheet2.png';
-	      charImg.onload = () => {
-	      	console.log('chargée');
-	        // ctx.drawImage(charImg, 0, 0);
-	      };
+	charImg.src = '../img/spritesheet2.png';
+	charImg.onload = () => {
+		console.log('charImg chargée');
+	// ctx.drawImage(charImg, 0, 0);
+	};
 
-	      // On charge une autre image
-	      tileFloorHouseImg.src = '../img/complete-spritesheet.png';
-	      tileFloorHouseImg.onload = () => {
-	      	console.log('chargée');
-	        // ctx.drawImage(charImg, 0, 0);
-	      };
+	// On charge une autre image
+	tileFloorHouseImg.src = '../img/complete-spritesheet.png';
+	tileFloorHouseImg.onload = () => {
+		console.log('tileFloorHouseImg chargée');
+	// ctx.drawImage(charImg, 0, 0);
+	};
 
-	    // On charge une autre image
-	    tileChairHouseImg.src = '../img/complete-spritesheet.png';
-	    tileChairHouseImg.onload = () => {
-	    console.log('chargée');
-	    // ctx.drawImage(charImg, 0, 0);
-	    };
+	// On charge une autre image
+	tileChairHouseImg.src = '../img/complete-spritesheet.png';
+	tileChairHouseImg.onload = () => {
+	console.log('tileChairHouseImg chargée');
+	// ctx.drawImage(charImg, 0, 0);
+	};
+
+	// On charge une autre image
+	backgroundImg.src = '../img/background.png';
+	backgroundImg.onload = () => {
+	console.log('backgroundImg chargée');
+	// ctx.drawImage(charImg, 0, 0);
+	};
+
+
+
 }
 loadImage();
 
 
 
 // Méthode pour dessin le fond
-const drawBackground = () => {
+/*const drawBackground = () => {
 	ctx.rect(0, 0, innerWidth, innerHeight);
     ctx.fillStyle = "#6AA34D";
     ctx.fill(); 
+}*/
+
+// Dessine l'image de fond
+const drawBackground = ()=> {
+	
+	ctx.drawImage(backgroundImg, 0 , 0 , 640 , 360, 0 , 0, stage.width , stage.height);
 }
+
+
+
 
 // Dessine le parquet au sol
 const drawFloor = ()=> {
@@ -176,8 +275,35 @@ const drawAll = () => {
 
 
 
-drawFloor();
-drawChair();
+//drawFloor();
+// drawChair();
+drawBackground();
+
+
+if(checkCollision(obstacle, hero)){
+	if(hero.faceY === 22){ // si regarde en haut
+		hero.speedY = 0;
+		hero.y +=10;
+		hero.centerX = ((hero.x + hero.width) - (hero.width / 2));
+ 		hero.centerY = ((hero.y + hero.height) - (hero.height / 2));
+	} else if(hero.faceY === 120) { // Si regarde à droite
+		hero.speedX = 0;
+		hero.x -= 10;
+		hero.centerX = ((hero.x + hero.width) - (hero.width / 2));
+ 		hero.centerY = ((hero.y + hero.height) - (hero.height / 2));
+	} else if (hero.faceY === 310) {// si regarde à gauche
+		hero.speedX = 0;
+		hero.x +=10;
+		hero.centerX = ((hero.x + hero.width) - (hero.width / 2));
+ 		hero.centerY = ((hero.y + hero.height) - (hero.height / 2));
+	} else if (hero.faceY === 210) { // si regarde en bas
+		hero.speedY = 0;
+		hero.y -= 10;
+		hero.centerX = ((hero.x + hero.width) - (hero.width / 2));
+ 		hero.centerY = ((hero.y + hero.height) - (hero.height / 2));
+	}
+};
+
 hero.drawHero();
 
 }
